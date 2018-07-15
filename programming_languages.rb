904864b6835594f7b2ead3cf91e7cd2e337909c7
casey van-roose <kcvanroose@gmail.com>
2018-07-15 18:14:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_array = []
+  
+  languages.each do |known, lang|
+    lang.each do |l, type|
+      type.each do |i|
+    
+    new_hash = {}
+    new_hash[l] = type
+  
+
+    new_array.push(known)
+      end
+    end
+  end
+  
+  new_hash
+
+
+ binding.pry 
 end
